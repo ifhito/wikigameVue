819f682f5a687cf19cb312e7df021ac058fdc680
ifhito <hito01010101@gmail.com>
2022-10-04 22:57:03 +0900

    add test of view

--- src/test/WikiPage.spec.ts
+++ src/test/WikiPage.spec.ts
@@ -8,12 +8,4 @@ describe("スタート画面のテスト", () => {
 
 describe("実際のゲーム画面のテスト", () => {});
 
-describe("エラー画面のテスト");
-
-describe("websocketのonmessageのテスト", () => {
-  test("action: errorが送られてきたとき", () => {});
-  test("action: subscribedが送られてきたとき", () => {});
-  test("action: start_gameが送られてきたとき", () => {});
-  test("action: send_urlが送られてきたとき", () => {});
-  test("action: decied_winnerが送られてきたとき", () => {});
-});
+describe("エラー画面のテスト", () => {});
