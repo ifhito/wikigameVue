d9427c0d177b0db0c997c7aacfe4bc88e9902d74
ifhito <hito01010101@gmail.com>
2022-10-05 23:41:21 +0900

    fix: TODOの追加

--- src/customHooks.spec.ts
+++ src/customHooks.spec.ts
@@ -3,7 +3,7 @@ import { useFixHTML } from "./customHooks";
 import { nextTick, ref } from "vue";
 
 describe("test of useFixHTML", () => {
-  test("HTMLを入力したときに、body, title, aListが正しく取得できること", async () => {
+  test("HTMLを入力したときに、body, title, aListが正しく取得できること", () => {
     const HTMLBody = ref(
       '<!DOCTYPE html><html lang="ja"><head>  <meta charset="UTF-8" />  <link rel="icon" href="/favicon.ico" />  <meta name="viewport" content="width=device-width, initial-scale=1.0" />  <title>WikiGame</title></head><body>  <h1 id="firstHeading">test</h1><div id="mw-content-text">testがこれです<a href="https://xxxxxxxhogehoge.cotest">test</a>、それでテストになりそうなことが喜びと悲しみなんですよ.<a href="https://teste.ssss">yuuuutest</a>とはなんなのだろうか?<div class="reference">Reference</div></div></body></html>'
     );
@@ -19,5 +19,9 @@ describe("test of useFixHTML", () => {
 });
 
 describe("test of useDecideAction", () => {
-  test("");
+  test("errorのアクションが来た際に、errorStatusがtrueになり、errorMessageにエラーメッセージが入る", () => {});
+  test("subscribedのアクションが来た際に、subscribeRefのオブジェクトの値が渡され、変数に入ること", () => {});
+  test("start_gameのアクションが来た際に、startGameRef変数に値が入ること", () => {});
+  test("send_urlのアクションが来た際に、startGameRef変数に値が入ること", () => {});
+  test("decied_winnerのアクションが来た際に、deciedWinnerRefに値が入ること", () => {});
 });
