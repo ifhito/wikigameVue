c13f8cb658b105e23d36259d42617516b020aef8
ifhito <hito01010101@gmail.com>
2022-10-09 12:34:27 +0900

    feat: add button atoms

---
+++ src/components/Atoms/Button.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { ref, defineProps, PropType} from 'vue'
+    interface Props {
+        onClick: (disabled:boolean | null) => void;
+    };
+    const props = defineProps<Props>()
+</script>
+    
+<template>
+    <button  class="tool tooltip">
+    <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+</style>
