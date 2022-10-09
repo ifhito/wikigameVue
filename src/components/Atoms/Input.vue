f770e9fa99eeeca314ddb0a31f6c93b8613c622b
ifhito <hito01010101@gmail.com>
2022-10-09 12:34:40 +0900

    feat: add input atoms

---
+++ src/components/Atoms/Input.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { ref, defineProps, PropType, Ref} from 'vue'
+    interface Props {
+        inputId: string;
+        vModel: Ref<string>;
+        type:string;
+    };
+    const props = defineProps<Props>()
+</script>
+    
+<template>
+    <input :id="inputId" :type="type" v-model="vModel" />
+</template>
+
+<style scoped>
+</style>
