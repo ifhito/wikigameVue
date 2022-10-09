f696679e2c44bc60c8bdaef82911abe8e57d0e8a
ifhito <hito01010101@gmail.com>
2022-10-09 12:58:34 +0900

    feat: add PlayerInfo organism

---
+++ src/components/Organisms/PlayerInfo.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    import { ref, defineProps, PropType, Ref} from 'vue'
+    interface Props {
+        answer: string;
+        nowName: string;
+        submitUser: string[];
+        myName: string;
+    };
+    const props = defineProps<Props>()
+</script>
+    
+<template>
+    <div>あなたの答え: {{answer}}</div>
+    <div>現在のターン: {{nowName}}</div>
+    <ul>
+        <li v-for="user in submitUser" :key="user">
+            {{user}}<span v-if="user === myName">あなた</span>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+</style>
