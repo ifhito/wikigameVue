e499a170d42a648e9bdd1e3c210fcefe3f3c8c54
ifhito <hito01010101@gmail.com>
2022-10-09 20:16:03 +0900

    feat: add makeRoom pages

---
+++ src/components/Pages/makeRoom.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+// TODO: 通信が切れたらクローズする
+import { ref, onMounted} from 'vue';
+import { useRouter } from 'vue-router'
+import WithInput from '../Moleculs/WithInput.vue';
+const name = ref<string>('')
+const roomNameBool = ref<boolean>(false)
+const roomName = ref<string>('')
+const router = useRouter()
+const onClick = () => {
+  
+  router.push({ path: '/game', query: { name: `${name.value}_${Math.random().toString(32).substring(2)}`, roomName:  roomName.value === ''?Math.random().toString(32).substring(2):roomName.value} } )
+}
+</script>
+
+<template>
+    <form
+      novalidate
+      class="ui form"
+    >
+      <div class="field">
+        <WithInput label-for="name">
+          <template v-slot:label>名前</template>
+          <template v-slot:formContents>
+            <input
+              id="name"
+              v-model="name"
+              type="text"
+            />
+          </template>
+        </WithInput>
+        <WithInput v-if="roomNameBool" label-for="roomName">
+          <template v-slot:label>部屋のID</template>
+          <template v-slot:formContents>
+            <input
+              id="roomName"
+              v-model="roomName"
+              type="text"
+            />
+          </template>
+        </WithInput>
+      </div>
+      <button
+        type="button"
+        class="ui button"
+        @click="onClick"
+      >
+        Submit
+      </button>
+      <button
+        v-if="!roomNameBool"
+        type="button"
+        @click="()=>roomNameBool = true"
+      >すでにある部屋に入る</button>
+    </form>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
