52643558fac5ff55889eac663ad29f8c53f62ac4
ifhito <hito01010101@gmail.com>
2022-10-09 12:49:53 +0900

    feat: add sentenceBody organisms

---
+++ src/components/Organisms/SentenceBody.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+    import { ref, defineProps, PropType, Ref} from 'vue'
+    interface Props {
+        body: (string | null)[];
+        myName: string;
+        nowName: string;
+        aList: string[];
+        delA: string[];
+        onClickSendText: (e:any) => void;
+    };
+    const props = defineProps<Props>()
+</script>
+    
+<template>
+    <template v-for="i in body.length" :key="`num_${i}`">
+        <span v-html="body[i]"></span>
+        <span v-if="myName=== nowName">
+            <button v-if="!(delA.includes(aList[i]) || /[*.]/.test(aList[i]))" :value="aList[i]" @click="onClickSendText">
+                {{aList[i]}}
+            </button>
+        </span>
+        <span v-else>
+           <span v-if="!(delA.includes(aList[i]) || /[*.]/.test(aList[i]))" :value="aList[i]">{{aList[i]}}</span>
+        </span>
+    </template>
+</template>
+
+<style scoped>
+</style>
