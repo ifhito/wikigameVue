823176f105f02264a05e88b04003473324f289f8
ifhito <hito01010101@gmail.com>
2022-10-09 12:35:04 +0900

    feat: add withInput moleculs

---
+++ src/components/Moleculs/WithInput.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+    import { ref, defineProps, PropType, Ref} from 'vue'
+    interface Props {
+        labelFor: string;
+    };
+    const props = defineProps<Props>()
+</script>
+    
+<template>
+    <label :for="labelFor"><slot name="name"></slot></label>
+    <slot name="formContents"></slot>
+</template>
+
+<style scoped>
+</style>
