d56b07434865a4c2dccf849c63618d14ad0d1a43
ifhito <hito01010101@gmail.com>
2022-10-09 20:15:16 +0900

    feat: add ui moleculs

---
+++ src/components/Moleculs/Ul.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    import { ref, defineProps, PropType, Ref} from 'vue'
+    interface Props {
+        submitUser: string[];
+        myName: string;
+    };
+    const props = defineProps<Props>()
+</script>
+    
+<template>
+    <ul>
+        <li v-for="user in submitUser" :key="user">
+            {{user}}<span v-if="user === myName">あなた</span>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+</style>
