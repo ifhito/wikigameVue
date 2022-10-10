3c4060b1a441249bdeda343630407cbb1857b660
ifhito <hito01010101@gmail.com>
2022-10-10 23:04:12 +0900

    refactor: fix button test

--- src/test/Atoms/Button.spec.ts
+++ src/test/Atoms/Button.spec.ts
@@ -1,22 +1,24 @@
 import { mount } from "@vue/test-utils";
 import Button from "../../components/Atoms/Button.vue";
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, beforeEach } from "vitest";
 import { ref } from "vue";
 
 describe("ボタンコンポーネントのテスト", () => {
-  test("ボタン要素がレンダリングされること", async () => {
-    const text = ref("");
-    const button = mount(Button, {
-      props: {
-        buttonType: "button",
-        buttonValue: "test",
-        onClick: (e: any) => (text.value = "test"),
-      },
-      slots: {
-        default: "ボタン",
-      },
-    });
+  const text = ref("");
+  const button = mount(Button, {
+    props: {
+      buttonType: "button",
+      buttonValue: "test",
+      onClick: (e: any) => (text.value = "test"),
+    },
+    slots: {
+      default: "ボタン",
+    },
+  });
+  test("ボタン要素がレンダリングされること", () => {
     expect(button.html()).toContain("ボタン");
+  });
+  test("ボタンをクリックすると値が変更されること", async () => {
     await button.trigger("click");
     expect(text.value).toBe("test");
   });
