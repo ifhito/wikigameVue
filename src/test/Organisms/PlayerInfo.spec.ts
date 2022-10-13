9f8e1debfad63ac21516a1c64a15d6168adad2a9
ifhito <hito01010101@gmail.com>
2022-10-13 22:56:02 +0900

    test: add playerinfo componants

---
+++ src/test/Organisms/PlayerInfo.spec.ts
@@ -0,0 +1,20 @@
+import { mount, shallowMount } from "@vue/test-utils";
+import PlayerInfo from "../../components/Organisms/PlayerInfo.vue";
+import { describe, test, expect, beforeEach } from "vitest";
+import { ref } from "vue";
+
+describe("withinputのテスト", () => {
+  const wrapper = shallowMount(PlayerInfo, {
+    props: {
+      answer: "test",
+      nowName: "testUser",
+      submitUser: ["testUser", "testUser2", "testUser3"],
+      myName: "testUser2",
+    },
+  });
+  test("表示されたコンテンツが予測した結果になっている", async () => {
+    console.log(wrapper.html());
+    expect(wrapper.html()).toContain("<div>あなたの答え: test</div>");
+    expect(wrapper.html()).toContain("<div>現在のターン: testUser</div>");
+  });
+});
