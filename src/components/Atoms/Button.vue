5c4e174f5d505f2072285376b42f30e0d63f8c24
ifhito <hito01010101@gmail.com>
2022-10-23 21:14:06 +0900

    feat: button要素にidを設定できるようにする

--- src/components/Atoms/Button.vue
+++ src/components/Atoms/Button.vue
@@ -2,14 +2,15 @@
     import { ref, defineProps, PropType} from 'vue'
     interface Props {
         buttonType: "button" | "submit" | "reset" | undefined;
-        buttonValue: string;
+        buttonValue?: string;
+        buttonId?: string;
         onClick: (e:any) => void;
     };
     const props = defineProps<Props>()
 </script>
     
 <template>
-    <button class="tool tooltip" :value="buttonValue" @click="onClick" :type="buttonType">
+    <button class="tool tooltip" :id="buttonId" :value="buttonValue" @click="onClick" :type="buttonType">
     <slot></slot>
     </button>
 </template>
